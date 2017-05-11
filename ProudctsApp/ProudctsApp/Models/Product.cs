df85bd1d41105432db5feded1aac3e68eaca1ff6
keerthana10 <keerthana.b@sysfore.com>
2017-05-11 11:54:01 +0530

    Third -First commit
    Third -Second Commit
    
    Third-Third Commit

--- ProudctsApp/ProudctsApp/Models/Product.cs
+++ ProudctsApp/ProudctsApp/Models/Product.cs
@@ -7,9 +7,18 @@ namespace ProudctsApp.Models
 {
     public class Product
     {
-        public int Id { get; set; }
+        //public int Id { get; set; }
         public string Name { get; set; }
         public string Category { get; set; }
         public decimal Price { get; set; }
+//First Commit
+public decimal Quantity1 { get; set; }
+public decimal Quantity2 { get; set; }
+//Second Commit
+public decimal Quality1 { get; set; }
+public decimal Quality2 { get; set; }
+//Third Commit
+public decimal Volume1 { get; set; }
+public decimal Volume2 { get; set; }
     }
 }
